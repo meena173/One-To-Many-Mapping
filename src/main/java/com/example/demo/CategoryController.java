d3e42af6364f02edee8b7b1dc180653427c0d089
meena173 <meenaarunveerappa908@gmail.com>
2024-08-14 13:53:12 +0530

    One to Many

--- src/main/java/com/example/demo/CategoryController.java
+++ src/main/java/com/example/demo/CategoryController.java
@@ -4,68 +4,100 @@ import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/cateories")
+@RequestMapping("/api/categories")
 public class CategoryController 
 {
-	@Autowired
-	CategoryRepo cr;	
-	
-	@GetMapping
-	public Page<Category> getAll(@PageableDefault(sort="name", direction=Sort.Direction.ASC)Pageable page)
-	{		
-		return cr.findAll(page);
-	}	
-	
-	@GetMapping("/{id}")
-	public Category getId(@PathVariable int id)
+	@GetMapping("/all")
+	public List<Category> getAllCategories() 
 	{
-		return cr.findById(id).orElse(null);
+	     System.out.println("Fetching all categories");
+	     List<Category> categories = cr.findAll();
+	    
+	    System.out.println("Categories fetched: " + categories.size());	    
+	    return categories;
 	}
 
-	@PostMapping
-	public Category saveAll(@RequestBody Category c)
-	{
-		Category ce = new Category();
-		ce.setName(c.getName());
-		
-		List<Product> list = new ArrayList<>();
-		
-		for(Product p : c.getProduct() )
-		{
-			
-			Product pe = new Product();
-			pe.setCategory(ce);
-			pe.setName(p.getName());
-			
-			list.add(pe);
-		}
-		ce.setProduct(list);
-		
-		return cr.save(ce);
-		
-	}	
-	
-	@PutMapping("/{id}")
-	public Category putId(@PathVariable int id, @RequestBody Category c)
-	{
-		Category ce = cr.findById(id).orElseThrow();
-		ce.setProduct(c.getProduct());
-		ce.setName(c.getName());
-		
-		return cr.save(ce);	
-		
-	}	
-	
-	@DeleteMapping("/{id}")
-	public void deleteId(@PathVariable int id)
-	{
-		cr.deleteById(id);
-	}
-	
+    @Autowired
+    CategoryRepo cr;
+
+    @GetMapping("/page/{pageNo}/{pageSize}")
+    public List<Category> getPaginated(@PathVariable int pageNo, @PathVariable int pageSize) 
+    {
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        Page<Category> pagedResult = cr.findAll(pageable);
+        
+        return pagedResult.hasContent() ? pagedResult.getContent() : new ArrayList<Category>();
+    }
+
+    @GetMapping("/sort")
+    public List<Category> getAllSorted(@RequestParam String field, @RequestParam String direction) 
+    {
+        Sort.Direction sortDirection = direction.equalsIgnoreCase("desc") ? Sort.Direction.DESC : Sort.Direction.ASC;
+        return cr.findAll(Sort.by(sortDirection, field));
+    }
+
+    @GetMapping("/page/{pageNo}/{pageSize}/sort")
+    public List<Category> getPaginatedAndSorted(@PathVariable int pageNo, @PathVariable int pageSize, 
+                                                @RequestParam String sortField, @RequestParam String sortDir) 
+    {
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
+                    Sort.by(sortField).descending();
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+        Page<Category> pagedResult = cr.findAll(pageable);
+        
+        return pagedResult.hasContent() ? pagedResult.getContent() : new ArrayList<Category>();
+    }
+
+   
+    @GetMapping("/{id}")
+    public Category getId(@PathVariable int id)
+    {
+        return cr.findById(id).orElse(null);
+    }
+
+    
+    @PostMapping
+    public Category saveAll(@RequestBody Category c) 
+    {
+        Category ce = new Category();
+        ce.setName(c.getName());
+
+        List<Product> list = new ArrayList<>();
+
+        for (Product p : c.getProduct()) 
+        {
+            Product pe = new Product();
+            pe.setCategory(ce);
+            pe.setName(p.getName());
+
+            list.add(pe);
+        }
+        ce.setProduct(list);
+
+        return cr.save(ce);
+    }
+
+  
+    @PutMapping("/{id}")
+    public Category putId(@PathVariable int id, @RequestBody Category c) 
+    {
+        Category ce = cr.findById(id).orElseThrow();
+        ce.setProduct(c.getProduct());
+        ce.setName(c.getName());
+
+        return cr.save(ce);
+    }
+
+   
+    @DeleteMapping("/{id}")
+    public void deleteId(@PathVariable int id)
+    {
+        cr.deleteById(id);
+    }
 }
--- src/main/java/com/example/demo/ProductController.java
+++ src/main/java/com/example/demo/ProductController.java
@@ -1,50 +1,86 @@
 package com.example.demo;
+
+import java.util.List;
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
-public class ProductController
-{	
-	@Autowired
-	ProductRepo cr;	
-	
-	@GetMapping
-	public Page<Product> getAll(@PageableDefault(sort="name", direction=Sort.Direction.ASC)Pageable page)
-	{		
-		return cr.findAll(page);
-	}
-		
-	@GetMapping("/{id}")
-	public Product getId(@PathVariable int id)
-	{
-		return cr.findById(id).orElse(null);
-	}
+public class ProductController 
+{
 
-	@PostMapping
-	public Product saveAll(@RequestBody Product ce)
-	{		
-		return cr.save(ce);		
-	}
-	
-	@PutMapping("/{id}")
-	public Product putId(@PathVariable int id, @RequestBody Product c)
-	{
-		Product ce = cr.findById(id).orElseThrow();
-		ce.setCategory(c.getCategory());
-		ce.setName(c.getName());
-		
-		return cr.save(ce);
-				
-	}	
-	
-	@DeleteMapping("/{id}")
-	public void dleteId(@PathVariable int id)
-	{
-		cr.deleteById(id);
-	}	
+    @Autowired
+    ProductRepo pr;
+    
+    @GetMapping("/all")
+    public List<Product> getAllProducts() 
+    {
+        return pr.findAll();
+    }
+    
+    @GetMapping("/page/{pageNo}/{pageSize}")
+    public List<Product> getPaginated(@PathVariable int pageNo, @PathVariable int pageSize) 
+    {
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        Page<Product> pagedResult = pr.findAll(pageable);
+        
+        return pagedResult.hasContent() ? pagedResult.getContent() : new ArrayList<Product>();
+    }
+
+  
+    @GetMapping("/sort")
+    public List<Product> getAllSorted(@RequestParam String field, @RequestParam String direction)
+    {
+        Sort.Direction sortDirection = direction.equalsIgnoreCase("desc") ? Sort.Direction.DESC : Sort.Direction.ASC;
+        return pr.findAll(Sort.by(sortDirection, field));
+    }
+
+
+    @GetMapping("/page/{pageNo}/{pageSize}/sort")
+    public List<Product> getPaginatedAndSorted(@PathVariable int pageNo, @PathVariable int pageSize, 
+                                               @RequestParam String sortField, @RequestParam String sortDir) 
+    {
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
+                    Sort.by(sortField).descending();
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+        Page<Product> pagedResult = pr.findAll(pageable);
+        
+        return pagedResult.hasContent() ? pagedResult.getContent() : new ArrayList<Product>();
+    }
+
+
+    @GetMapping("/{id}")
+    public Product getId(@PathVariable int id) 
+    {
+        return pr.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Product saveAll(@RequestBody Product p) 
+    {
+        return pr.save(p);
+    }
+
+    @PutMapping("/{id}")
+    public Product putId(@PathVariable int id, @RequestBody Product p) 
+    {
+        Product existingProduct = pr.findById(id).orElseThrow();
+        existingProduct.setCategory(p.getCategory());
+        existingProduct.setName(p.getName());
+
+        return pr.save(existingProduct);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void deleteId(@PathVariable int id) 
+    {
+        pr.deleteById(id);
+    }
 }
